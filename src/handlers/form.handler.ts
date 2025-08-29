0e60401a91f066fe783512bd4dd20085aa683f8c
alberto.mallar <alberto.mallar@gmail.com>
2025-08-29 17:22:14 +0000

    move git folder into root folder

--- src/handlers/form.handler.ts
+++ src/handlers/form.handler.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import {
   getFormController,
   postFormController,
@@ -14,13 +14,16 @@ export const getFormHandler = async (req: Request, res: Response) => {
   }
 };
 
-export const postFormHandler = async (req: Request, res: Response) => {
+export const postFormHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     let formData = req.body;
     const response = await postFormController(formData);
-    res.send(response);
+    res.status(201).json({ success: true, data: response });
   } catch (error) {
-    console.error("Error in postFormHandler:", error);
-    res.status(500).json({ message: "internal server error" });
+    next(error);
   }
 };
